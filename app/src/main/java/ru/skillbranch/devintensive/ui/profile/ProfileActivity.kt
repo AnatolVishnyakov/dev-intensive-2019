7f0840469f4d52d6df4c859d13c06f80001e1a8b
Anatol V <malfenius@gmail.com>
2019-07-26 22:58:26 +0300

    hometask_4. Implements UI for video.

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,25 +1,12 @@
-package ru.skillbranch.devintensive
+package ru.skillbranch.devintensive.ui.profile
 
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.skillbranch.devintensive.extensions.hideKeyboard
-import ru.skillbranch.devintensive.models.Bender
+import ru.skillbranch.devintensive.R
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var benderImage: ImageView
-    lateinit var textTxt: TextView
-    lateinit var messageEt: EditText
-    lateinit var sendBtn: ImageView
-    lateinit var benderObj: Bender
+class ProfileActivity : AppCompatActivity() {
 
     /**
      * Вызывается при первом создании или перезапуске Activity.
@@ -36,30 +23,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-//        benderImage = findViewById(R.id.iv_bender)
-        benderImage = iv_bender
-        textTxt = tv_text
-        messageEt = et_message
-        sendBtn = iv_send
-
-        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
-        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
-
-        benderObj = Bender(
-            status = Bender.Status.valueOf(status),
-            question = Bender.Question.valueOf(question)
-        )
-
-        Log.d("M_MainActivity", "onCreate $status $question")
-        val (r, g, b) = benderObj.status.color
-        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-
-        textTxt.text = benderObj.askQuestion()
-        sendBtn.setOnClickListener(this)
-
-        setOnAction()
+        setContentView(R.layout.activity_profile)
     }
 
     /**
@@ -158,17 +122,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Log.d("M_MainActivity", "onDestroy")
     }
 
-    override fun onClick(v: View?) {
-        if (v?.id == R.id.iv_send) {
-            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
-            messageEt.setText("")
-
-            val (r, g, b) = color
-            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
-            textTxt.text = phrase
-        }
-    }
-
     /**
      * Этот метод сохраняет состояние представления в Bundle.
      * Для API Level < 28 (Android P), этот метод будет выполняться
@@ -181,33 +134,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
      * */
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
-
-        outState.putString("STATUS", benderObj.status.name)
-        outState.putString("QUESTION", benderObj.question.name)
-    }
-
-    private fun setOnAction() {
-        messageEt.setOnEditorActionListener { _, actionId, _ ->
-            return@setOnEditorActionListener when (actionId) {
-                EditorInfo.IME_ACTION_DONE -> {
-                    sendBtn.performClick()
-                    hideKeyboard()
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-
-    private fun initEvents() {
-        // При нажатии на изображение, клавиатуру скрывается
-        val clickListener = View.OnClickListener {
-            if (it == benderImage) {
-                this.hideKeyboard()
-//                Log.d("M_MainActivity", this.isKeyboardOpen().toString())
-            }
-        }
-        benderImage.setOnClickListener(clickListener)
     }
 }
