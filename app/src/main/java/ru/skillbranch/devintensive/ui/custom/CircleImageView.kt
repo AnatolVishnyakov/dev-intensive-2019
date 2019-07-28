48f69ae0002bf5117697f92770512b5e4e63ae28
Anatol V <malfenius@gmail.com>
2019-07-28 15:06:27 +0300

    hometask_4. Implements CircleImageView.

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -0,0 +1,20 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+// TODO implements
+class CircleImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+
+    init {
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+}
