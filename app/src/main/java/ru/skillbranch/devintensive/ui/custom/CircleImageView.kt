ac681af4371ebbb6eeda27332631de5157d9e53a
Anatol V <malfenius@gmail.com>
2019-07-31 21:43:54 +0300

    hometask_4. Task about create circle avatar.

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -1,17 +1,54 @@
 package ru.skillbranch.devintensive.ui.custom
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.annotation.Dimension
+import ru.skillbranch.devintensive.R
+
 
 // TODO implements
 class CircleImageView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ImageView(context, attrs, defStyleAttr) {
 
+    companion object {
+        private const val DEFAULT_COLOR = Color.WHITE
+        private const val DEFAULT_BORDER_WIDTH = 2
+    }
+
+    private var borderColor = DEFAULT_COLOR
+    private var borderWidth = DEFAULT_BORDER_WIDTH
+
     init {
+        if (attrs != null) {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
+            borderColor = a.getColor(R.styleable.CircleImageView_cv_borderColor, DEFAULT_COLOR)
+            a.recycle() // высвобождает ресурс и не обращается больше к нему
+        }
+    }
+
+    @Dimension
+    fun getBorderWidth(): Int {
+        return borderWidth
+    }
+
+    fun setBorderWidth(@Dimension dp: Int) {
+        borderWidth = dp
+    }
+
+    fun getBorderColor(): Int {
+        return borderColor
+    }
+
+    fun setBorderColor(hex: String) {
+        borderColor = Color.parseColor(hex)
+    }
+
+    fun setBorderColor(@ColorRes colorId: Int) {
+        borderColor = colorId
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
